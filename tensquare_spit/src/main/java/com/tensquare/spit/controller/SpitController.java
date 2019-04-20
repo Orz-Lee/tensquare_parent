e94274f981e4794158eb2d8cccf51cd720cab3e5
RigidLee <mr.learnerli@gmail.com>
2019-04-20 15:15:25 +0800

    <Committed By Lee>

---
+++ tensquare_spit/src/main/java/com/tensquare/spit/controller/SpitController.java
@@ -0,0 +1,104 @@
+package com.tensquare.spit.controller;
+
+import com.tensquare.spit.pojo.Spit;
+import com.tensquare.spit.service.SpitService;
+import entity.PageResult;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/spit")
+public class SpitController {
+
+    @Autowired
+    private SpitService spitService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 点赞
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/thumbup/{id}", method = RequestMethod.PUT)
+    public Result updateThumbup(@PathVariable String id) {
+        //判断当前用户是否已经点赞，但是现在我们没有做认证，暂时先把userid写死
+        String userid = "1011";
+        //判断当前用户是否已经点赞
+        //redisTemplate.delete("thumbup_" + userid + "_" + id);
+        if (redisTemplate.opsForValue().get("thumbup_" + userid + "_" + id) != null) {
+            return new Result(false, StatusCode.REPERROR, "不能重复点赞");
+        }
+        spitService.updateThumbup(id);
+        redisTemplate.opsForValue().set("thumbup_" + userid + "_" + id, 1);
+        return new Result(true, StatusCode.OK, "点赞成功");
+    }
+
+    /**
+     * 根据上级ID查询吐槽分页数据
+     * @param page
+     * @param size
+     * @return
+     */
+    @RequestMapping(value = "/comment/{parentId}/{page}/{size}", method = RequestMethod.GET)
+    public Result findByParentid(@PathVariable String parentId, @PathVariable int page, @PathVariable int size) {
+        Page<Spit> pageList = spitService.findByParentid(parentId, page, size);
+        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Spit>(pageList.getTotalElements(), pageList.getContent()));
+    }
+
+    /**
+     * 查询全部数据
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功", spitService.findAll());
+    }
+
+    /**
+     * 根据ID查询
+     * @param id ID
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String id) {
+        return new Result(true, StatusCode.OK, "查询成功", spitService.findById(id));
+    }
+
+    /**
+     * 增加
+     * @param spit
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public Result save(@RequestBody Spit spit) {
+        spitService.save(spit);
+        return new Result(true, StatusCode.OK, "增加成功");
+    }
+
+    /**
+     * 修改
+     * @param spit
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Result update(@RequestBody Spit spit, @PathVariable String id) {
+        spit.set_id(id);
+        spitService.update(spit);
+        return new Result(true, StatusCode.OK, "修改成功");
+    }
+
+    /**
+     * 删除
+     * @param id
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String id) {
+        spitService.deleteById(id);
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+}
